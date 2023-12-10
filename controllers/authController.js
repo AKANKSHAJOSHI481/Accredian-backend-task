1f057901213b1cd99962a7a80dcc4b1d66af9126
AKANKSHAJOSHI481 <joshiakanksha481@gmail.com>
2023-12-10 15:13:55 +0530

    DB server updates

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,12 @@
 const mysql = require("mysql");
 const bcrypt = require("bcrypt");
-
-
+require("dotenv").config();
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Akanksha@481",
-  database: "accredian_database",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
 connection.connect((err) => {
